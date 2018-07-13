d12797120751bd7162ad9a775dcd8e3170cca9b1
qikke <qikke1995@126.com>
2018-07-13 10:15:44 +0800

    完善了注册、登陆功能，增加了cookie验证

--- server.js
+++ server.js
@@ -27,7 +27,20 @@ var server = http.createServer(function (request, response) {
 
 
   if (path === '/') {
-    var string = fs.readFileSync('./index.html', 'utf8')
+    let string = fs.readFileSync('./index.html', 'utf8')
+
+    let cookies = request.headers.cookie && request.headers.cookie.split(';')
+    let hash = {}
+    cookies && cookies.forEach((value) => {
+      let tempArr = value.split('=')
+      hash[tempArr[0]] = tempArr[1]
+    })
+    let detail = hash && dbSearch(hash.sign_in_account)
+    if (detail) {
+      string = string.replace('&&&&log&&&&', `你好${hash.sign_in_account}`)
+    } else {
+      string = string.replace('&&&&log&&&&', '请登录')
+    }
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
@@ -39,35 +52,77 @@ var server = http.createServer(function (request, response) {
     response.write(string)
     response.end()
   } else if (path === '/verification' && method.toUpperCase() === 'POST') {
-
     readBody(request).then((body) => {
-      let data = {}
-      let arr = body.split('&')
-      arr.forEach((value) => {
-        let tempArr = value.split('=')
-        data[tempArr[0]] = tempArr[1]
-      }) //分析数据
+      let data = stringParse(body) //分析数据
       let {
         account,
         password
       } = data
-      if (account === 'admin' && password === 'admin') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text;charset=utf-8')
-        response.write('success')
+      if (account.length >= 6 || password.length >= 6) {
+        console.log(dbSearch(account))
+        if (!dbSearch(account)) {
+          //数据库中没有该帐号 
+          response.statusCode = 200
+          response.setHeader('Content-Type', 'text;charset=utf-8')
+          response.write('success')
+          let dbData = JSON.parse(fs.readFileSync('./db', 'utf8'))
+          dbData.push({
+            account: account,
+            password: password
+          })
+          fs.writeFileSync('./db', JSON.stringify(dbData), 'utf8')
+        } else {
+          response.statusCode = 404
+          response.setHeader('Content-Type', 'text/json;charset=utf-8')
+          response.write(`{
+          "error":"该账号已存在"
+          }`)
+        }
       } else {
         response.statusCode = 404
         response.setHeader('Content-Type', 'text/json;charset=utf-8')
         response.write(`{
-          "error":"账号密码不匹配"
+          "error":"账号密码必须6位以上哦"
           }`)
       }
-
-
       response.end()
     })
 
-
+  } else if (path === '/log_in' && method.toUpperCase() === 'POST') {
+    readBody(request).then((body) => {
+      let data = stringParse(body) //分析数据
+      let {
+        account,
+        password
+      } = data
+      let detail = dbSearch(account)
+      if (detail) {
+        if (detail.password === password) {
+          response.statusCode = 200
+          //给用户一个cokkie
+          response.setHeader('Set-Cookie', `sign_in_account=${account}`)
+        } else {
+          response.statusCode = 400
+          response.setHeader('Content-Type', 'text/json;charset=utf-8')
+          response.write(`{
+          "error":"密码错误"
+          }`)
+        }
+      } else {
+        response.statusCode = 400
+        response.setHeader('Content-Type', 'text/json;charset=utf-8')
+        response.write(`{
+          "error":"帐号不存在"
+          }`)
+      }
+      response.end()
+    })
+  } else if (path === '/log_in') {
+    var string = fs.readFileSync('./log_in.html', 'utf8')
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
   } else {
     response.statusCode = 404
     response.end()
@@ -83,6 +138,26 @@ var server = http.createServer(function (request, response) {
         resolve(body)
       })
     })
+  }
+
+  function stringParse(string) {
+    let data = {}
+    let arr = string.split('&')
+    arr.forEach((value) => {
+      let tempArr = value.split('=')
+      data[tempArr[0]] = tempArr[1]
+    })
+    return data
+  }
+
+  function dbSearch(account) {
+    let dbData = JSON.parse(fs.readFileSync('./db', 'utf-8'))
+    for (let i = 0; i < dbData.length; i++) {
+      if (dbData[i].account === account) {
+        return dbData[i]
+      }
+    }
+    return false
   }
 })
 
